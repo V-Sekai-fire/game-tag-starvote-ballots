5d02c98978cb8aa9e840eef418c04f0d63974aea
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-02 09:20:24 -0800

    Add name_column argument to process_csv_file for improved CSV handling

--- game_tag_starvote_ballots.py
+++ game_tag_starvote_ballots.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def process_csv_file(csv_file_path, target_metric_column):
+def process_csv_file(csv_file_path, name_column, target_metric_column):
     ballots = []
     maximum_score = 1
 
@@ -16,7 +16,7 @@ def process_csv_file(csv_file_path, target_metric_column):
         reader = csv.DictReader(csvfile)
         for row in reader:
             try:
-                tags = ast.literal_eval(row["Tags"])
+                tags = ast.literal_eval(row[name_column])
                 target_metric_str = row[target_metric_column]
                 if (
                     target_metric_str.lower() == "null"
@@ -43,7 +43,13 @@ def main():
         "--csv_file_path",
         type=str,
         default="Visual Novel - Tag Explorer - GameDiscoverCo Plus.csv",
-        help="Path to the CSV file. The CSV should have columns:  Name, and a column for the target metric that can be converted to a integer from [1, Inf].",
+        help="Path to the CSV file. The CSV should have a name column and a column for the target metric that can be converted to a integer from [1, Inf].",
+    )
+    parser.add_argument(
+        "--name_column",
+        type=str,
+        default="Tags",
+        help="Column name for the tags.",
     )
     parser.add_argument(
         "--target_metric_column",
@@ -61,11 +67,13 @@ def main():
     args = parser.parse_args()
 
     ballots, maximum_score = process_csv_file(
-        args.csv_file_path, args.target_metric_column
+        args.csv_file_path, args.name_column, args.target_metric_column
     )
 
     results = starvote.election(
-        method=starvote.STAR_Voting if args.candidates < 2 else starvote.Allocated_Score_Voting,
+        method=starvote.STAR_Voting
+        if args.candidates < 2
+        else starvote.Allocated_Score_Voting,
         ballots=ballots,
         seats=args.candidates,
         tiebreaker=starvote.hashed_ballots_tiebreaker,
