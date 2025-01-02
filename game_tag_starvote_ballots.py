44faa081080e2a71b601dd31906eca89aa2620e8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-02 09:17:20 -0800

    Refactor CSV processing to remove unused variables and simplify return values

--- game_tag_starvote_ballots.py
+++ game_tag_starvote_ballots.py
@@ -1,8 +1,8 @@
-import starvote
 import csv
 import ast
 import logging
 import argparse
+import starvote
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,15 +10,11 @@ logger = logging.getLogger(__name__)
 
 def process_csv_file(csv_file_path, target_metric_column):
     ballots = []
-    decode_errors = 0
-    first_five_rows = []
     maximum_score = 1
 
     with open(csv_file_path, newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
-        for i, row in enumerate(reader):
-            if i < 5:
-                first_five_rows.append(row)
+        for row in reader:
             try:
                 tags = ast.literal_eval(row["Tags"])
                 target_metric_str = row[target_metric_column]
@@ -31,7 +27,6 @@ def process_csv_file(csv_file_path, target_metric_column):
                     target_metric = int(float(target_metric_str))
                 maximum_score = max(maximum_score, target_metric)
             except (ValueError, SyntaxError):
-                decode_errors += 1
                 continue
             ballot = {tag: target_metric for tag in tags}
             logger.debug(f"Ballot: {ballot}")
@@ -39,21 +34,7 @@ def process_csv_file(csv_file_path, target_metric_column):
 
     maximum_score = int(maximum_score)
 
-    return ballots, decode_errors, first_five_rows, maximum_score
-
-
-def print_summary(
-    csv_file_path,
-    target_metric_column,
-    candidates,
-    ballots,
-    decode_errors,
-):
-    logger.info(f"CSV file path: {csv_file_path}")
-    logger.info(f"Target metric column: {target_metric_column}")
-    logger.debug(f"Number of candidates: {candidates}")
-    logger.info(f"Total entries: {len(ballots)}")
-    logger.debug(f"Total decode errors: {decode_errors}")
+    return ballots, maximum_score
 
 
 def main():
@@ -79,18 +60,10 @@ def main():
 
     args = parser.parse_args()
 
-    ballots, decode_errors, first_five_rows, maximum_score = process_csv_file(
+    ballots, maximum_score = process_csv_file(
         args.csv_file_path, args.target_metric_column
     )
 
-    print_summary(
-        args.csv_file_path,
-        args.target_metric_column,
-        args.candidates,
-        ballots,
-        decode_errors,
-    )
-    
     results = starvote.election(
         method=starvote.STAR_Voting if args.candidates < 2 else starvote.Allocated_Score_Voting,
         ballots=ballots,
